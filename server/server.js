d447ae683a182fc720c2d388b955809ac0cd3fbb
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-08-22 17:19:22 +0100

    Work on different endpoints(CRUD) in server

--- server/server.js
+++ server/server.js
@@ -1,23 +1,66 @@
 const PORT = process.env.PORT || 8000;
 const express = require("express");
-const cors = require("cors")
+const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
 const app = express();
 const pool = require("./db");
-app.use((cors()))
+app.use(cors());
+app.use(express.json());
 
-// get all todos
+// Get all todos
 app.get("/todos/:userEmail", async (req, res) => {
-  const {userEmail} = req.params
+  const { userEmail } = req.params;
   try {
-    const todos = await pool.query(
+    const getToDo = await pool.query(
       "SELECT * FROM todos WHERE user_email = $1",
       [userEmail]
     );
-    res.json(todos.rows);
+    res.json(getToDo.rows);
   } catch (err) {
     console.error(err);
   }
 });
+
+// Create a new todo
+app.post("/todos/", async (req, res) => {
+  const { user_email, title, progress, date } = req.body;
+  const id = uuidv4();
+  try {
+    const addToDo = await pool.query(
+      "INSERT INTO todos (id, user_email, title, progress, date) VALUES($1, $2, $3, $4, $5)",
+      [id, user_email, title, progress, date]
+    );
+    res.json(addToDo);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//Edit a new todo
+app.put("/todos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { user_email, title, progress, date } = req.body;
+  try {
+    const updateToDo = await pool.query(
+      "UPDATE todos SET user_email=$1, title=$2, progress=$3, date=$4 WHERE id=$5",
+      [user_email, title, progress, date, id]
+    );
+    res.json(updateToDo);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Delete a todo
+app.delete("/todos/:id", async(req,res) => {
+  const {id} = req.params
+  try {
+    const deleteToDo = await pool.query("DELETE FROM todos WHERE id=$1", [id])
+    res.json(deleteToDo)
+  } catch (err) {
+    console.error(err)
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server is runnig on PORT ${PORT}`);
