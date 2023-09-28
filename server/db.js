6be00a70c4248f3e30bcfb41ff76ee21500499c8
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-09-28 19:18:52 +0100

    vercel

--- server/db.js
+++ server/db.js
@@ -1,13 +1,16 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// const pool = new Pool({
+//   user: process.env.USERNAME,
+//   password: process.env.PASSWORD,
+//   host: process.env.HOST,
+//   port: process.env.DBPORT,
+//   database: "todoapp",
+//   ssl: true,
+// });
 const pool = new Pool({
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  host: process.env.HOST,
-  port: process.env.DBPORT,
-  database: "todoapp",
-  ssl: true,
-});
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
 module.exports = pool;
